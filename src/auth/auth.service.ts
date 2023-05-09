9ebeb13d76a8b550dee6d7d1c13b0a54b3387a85
tnty <tnty.working@gmail.com>
2023-05-09 22:04:58 +0300

    feat: login method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,10 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { LoginUserDto } from './dto/login-user.dto';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcryptjs';
 import { User } from '../users/users.model';
-import { constants } from 'http2';
 
 @Injectable()
 export class AuthService {
@@ -14,19 +13,22 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async login(userDto: LoginUserDto) {}
+  async login(userDto: LoginUserDto) {
+    const user = await this.userService.getUserByEmail(userDto.email);
+    await this.validatePassword(userDto.password, user.password);
+    return this.generateToken(user);
+  }
 
   async registration(userDto: CreateUserDto) {
     const candidate = await this.userService.getUserByEmail(userDto.email);
 
     if (candidate) {
-      throw new HttpException(
+      throw new UnauthorizedException(
         'Пользователь с таким e-mail уже существует!',
-        HttpStatus.BAD_REQUEST,
       );
     }
 
-    const hashPassword: string = await bcrypt.hash(userDto.password, 5);
+    const hashPassword = await bcrypt.hash(userDto.password, 5);
     const user = await this.userService.createUser({
       ...userDto,
       password: hashPassword,
@@ -34,11 +36,19 @@ export class AuthService {
     return this.generateToken(user);
   }
 
-  async generateToken(user: User) {
+  private async generateToken(user: User) {
     const payload = { id: user.id, email: user.email, roles: user.roles };
-
     return {
       token: this.jwtService.sign(payload),
     };
+  }
+
+  private async validatePassword(password: string, hashPassword: string) {
+    const isPasswordCorrect = await bcrypt.compare(password, hashPassword);
+    if (!isPasswordCorrect) {
+      throw new UnauthorizedException(
+        'Пользователя с таким email или паролем не существует',
+      );
+    }
   }
 }
